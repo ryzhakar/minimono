2a0f5a47cc0c2782ddc6c7bc36e2778605ee8f7c
Arthur Ryzhak <ryzhakar@gmail.com>
2022-06-28 16:54:42 +0300

    Add docstrings

--- API/client.py
+++ API/client.py
@@ -7,12 +7,17 @@ from .api_call import MonoCaller
 class Client:
 
     def __init__(self, token, caller_class: Any = MonoCaller) -> None:
+        """Initialize clients request engine."""
+
         self.engine = caller_class(token)
         self.user = self.engine.make_request(UserInfoReq())
 
     def refreshUser(self):
+        """Refresh user info."""
+
         self.user = self.engine.make_request(UserInfoReq())
 
     def getRates(self) -> CurrInfoResp:
+        """Get currency rates."""
         return self.engine.make_request(CurrRateReq())
     
