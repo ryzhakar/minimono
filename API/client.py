0934b0e549b9af7671a16a6505759285a6ca25b4
Arthur Ryzhak <ryzhakar@gmail.com>
2022-07-08 17:42:34 +0300

    Fix default datetime objects timezones
    Fix type annotations to comply with 3.8

--- API/client.py
+++ API/client.py
@@ -1,5 +1,5 @@
-from datetime import datetime, timedelta
-from typing import Any
+from datetime import datetime, timedelta, timezone
+from typing import Any, List
 from .models import Account, CurrInfoResp, StatementReq, UserInfoResp, StatementResp, UserInfoReq, CurrRateReq, default_timeframe, TIMEBLOCK
 from .api_call import MonoCaller
 
@@ -27,7 +27,7 @@ class Client:
 
         return self.engine.make_request(CurrRateReq())
 
-    def availableAccounts(self) -> list[Account]:
+    def availableAccounts(self) -> List[Account]:
         """Get available accounts."""
 
         return [self.accounts.__getattribute__(x[0]) for x in list(self.user.accounts.dict().items()) if x[1] is not None]
@@ -35,7 +35,7 @@ class Client:
     def getStatement(
         self,
         account: Account,
-        from_time=(datetime.now() - timedelta(days=7)),
-        to_time=datetime.now()
+        from_time=(datetime.now(tz=timezone.utc) - timedelta(days=7)),
+        to_time=datetime.now(tz=timezone.utc)
         ) -> StatementResp:
         return account.getStatement(self.engine, fr=from_time, to=to_time)
